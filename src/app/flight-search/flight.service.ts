fd66c3b87e4bb4dd7f57e4c1512ef7edc0a5d299
mikezks <23002661+mikezks@users.noreply.github.com>
2024-04-16 15:04:23 +0200

    feat: use class

--- src/app/flight-search/flight.service.ts
+++ src/app/flight-search/flight.service.ts
@@ -1,28 +1,13 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Flight } from '../model/flight';
-import { HttpClient } from '@angular/common/http';
+import { DefaultFlightService } from './default-flight.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
+  useClass: DefaultFlightService
 })
-export class FlightService {
-  private http = inject(HttpClient);
-
-  find(from: string, to: string): Observable<Flight[]> {
-    const url = 'https://demo.angulararchitects.io/api/flight';
-    const params = { from, to };
-
-    return this.http.get<Flight[]>(url, { params });
-  }
-
-  save(flight: Flight): Observable<Flight> {
-    const url = 'https://demo.angulararchitects.io/api/flight';
-
-    const headers = {
-      Accept: 'application/json',
-    };
-
-    return this.http.post<Flight>(url, flight, { headers });
-  }
+export abstract class FlightService {
+  abstract find(from: string, to: string): Observable<Flight[]>;
+  abstract save(flight: Flight): Observable<Flight>;
 }
