6e506c8618663e0038a4b61476b25eb9bcf55c04
mikezks <23002661+mikezks@users.noreply.github.com>
2024-04-17 16:52:14 +0200

    feat: content projection

--- src/app/flight-card/flight-card.component.ts
+++ src/app/flight-card/flight-card.component.ts
@@ -9,19 +9,23 @@ import { NgClass } from '@angular/common';
     NgClass
   ],
   template: `
+    <!-- View of Flight Card -->
     <div
       [ngClass]="{ selected: selected() }"
       class="card"
     >
       <div class="card-header">
         <h2 class="card-title">
-          {{ item().from }} - {{ item().to }}</h2>
+          {{ item().from }} - {{ item().to }}
+        </h2>
+        <ng-content select=".content-title" />
       </div>
 
       <div class="card-body">
         <p>Flight No.: {{ item().id }}</p>
         <p>Date: {{ item().date }}</p>
         <p>Delayed: {{ item().delayed }}</p>
+        <ng-content />
         <p>
           <button (click)="toggleSelection()" class="btn btn-default">
             {{ selected() ? 'Remove' : 'Select' }}
