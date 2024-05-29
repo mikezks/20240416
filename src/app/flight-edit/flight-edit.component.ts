7f91b798c9bcb5c28c4277232531fa233d2a271c
mikezks <23002661+mikezks@users.noreply.github.com>
2024-05-29 10:03:13 +0200

    feat: flight edit td forms

---
+++ src/app/flight-edit/flight-edit.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { Flight } from '../model/flight';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-flight-edit',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatDialogModule,
+    JsonPipe
+  ],
+  templateUrl: './flight-edit.component.html',
+  styleUrl: './flight-edit.component.scss'
+})
+export class FlightEditComponent {
+  dialogRef = inject(MatDialogRef);
+  data = inject<{ flight: Flight }>(MAT_DIALOG_DATA);
+  flight = this.data.flight;
+
+  save(): void {
+    console.log(this.flight);
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+}
